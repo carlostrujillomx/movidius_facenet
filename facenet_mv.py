0a9029a96622af1cccd2399ab55c57e107f19ce2
Carlos-Trujillo <karlos15.02091@gmail.com>
2018-10-30 15:32:33 -0600

    protype ready

--- facenet_mv.py
+++ facenet_mv.py
@@ -13,6 +13,7 @@ class face_ncs():
         self.load_face_processing_files()
 
     def load_movidius_data(self):
+        print('loading movidius graph')
         devices = mvnc.EnumerateDevices()
         num_devices = len(devices)
         if num_devices == 0:
@@ -25,22 +26,27 @@ class face_ncs():
             graph_on_memory = f.read()
         
         self.graph = device.AllocateGraph(graph_on_memory)
+        print('load graph finished')
 
     def load_face_processing_files(self):
+        print('loading face processing data')
         haarcascade = 'haarcascade.xml'
         landmarks = 'landmarks.dat'
-        self.face_cascade = cv2.CascadeClassifie(haarcascade)
+        self.face_cascade = cv2.CascadeClassifier(haarcascade)
         self.predictor = dlib.shape_predictor(landmarks)
+        print('load face data finished')
 
-    def process_image(self, image):
+    def process_image(self, image_path):
         standard_width = 320
         standard_height = 240
-
+        image = cv2.imread(image_path)
         image_rsz = cv2.resize(image, (standard_width, standard_height))
         ret = self.detect_faces(image_rsz)
         if ret:
             encodings = self.get_encodings(image_rsz)
-            print(encodings)
+            #print(encodings)
+        
+        return image_rsz
 
     def detect_faces(self, image):
         self.faces = self.face_cascade.detectMultiScale(image, 1.1, 3)
@@ -57,6 +63,7 @@ class face_ncs():
             dlib_rect = dlib.rectangle(x1,y1,x2,y2)
             shape = self.predictor(image, dlib_rect)
             rotated = dlib.get_face_chip(image, shape)
+            cv2.rectangle(image, (x1,y1), (x2,y2), (0,0,255), 2)
             encodings.append(self.run_inference(rotated))
 
         return encodings
@@ -65,9 +72,9 @@ class face_ncs():
         NETWORK_WIDTH = 160
         NETWORK_HEIGHT = 160
         img = cv2.resize(input_image, (NETWORK_WIDTH, NETWORK_HEIGHT))
-        img = img.astype(np.float32)
+        img = img.astype(np.float16)
         self.graph.LoadTensor(img, None)
-        output, userobj = self.graph.GetResutl()
+        output, userobj = self.graph.GetResult()
 
         return output
 
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import sys
 import facenet_mv
+import time
+import cv2
 
 if __name__ == "__main__":
     image = sys.argv[1]
     facenet = facenet_mv.face_ncs()
-    facenet.process_image(image)
-    
+    tic = time.time()
+    processed_image = facenet.process_image(image)
+    toc = time.time()
+    elapsed = int((toc-tic)*1000)
+    print('elapsed_time=', elapsed)
+    cv2.imshow('image', processed_image)
+    cv2.waitKey()
